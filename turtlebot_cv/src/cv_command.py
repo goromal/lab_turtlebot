c1c30ca7da6883c5c628f9d4ed7185213e250f55
michael <michaeldavidfarrell@gmail.com>
2018-03-22 12:36:24 -0600

    tools for autonomous command and opencv processing

---
+++ turtlebot_cv/src/cv_command.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+from geometry_msgs.msg import Twist
+
+class CVControl:
+
+    def __init__(self):
+
+        # Turtlebot command publisher
+        self.cmd_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=10)
+        self.cmd = Twist()
+
+        # Image subscriber
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/decompressed_img", Image, self.img_callback)
+
+    def img_callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print e
+
+        # Draw circle on image
+        (rows,cols,channels) = cv_image.shape
+        if cols > 60 and rows > 60 :
+            cv2.circle(cv_image, (50,50), 10, 255)
+
+        cv2.imshow("Image window", cv_image)
+        cv2.waitKey(3)
+
+        # Send Velocity command to turtlebot
+        v = 0.0
+        w = 0.5
+        self.send_command(v, w)
+
+    def send_command(self, v, w):
+        # Put v, w commands into Twist message
+        self.cmd.linear.x = v
+        self.cmd.angular.z = w
+
+        # Publish Twist command
+        self.cmd_pub.publish(self.cmd)
+
+def main():
+
+    ctrl = CVControl()
+    rospy.init_node('image_converter')
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print "Shutting down"
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
